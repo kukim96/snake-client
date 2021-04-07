d09b6c8b7066d102c290bfb3f239f9ae4c9fea75
Kiup Kim <kukim96@gmail.com>
2021-04-07 15:08:18 -0600

    setup Stdin and Stdout

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', function (key) {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  const stdout = process.stdout;
+  if (key === '\u0003') {
+    stdout.write('Exited the game. Good bye now.\n');
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput();
