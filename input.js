626d856920ff9e6b5a90fd3f0daf36d3b172fc59
Kiup Kim <kukim96@gmail.com>
2021-04-07 15:44:22 -0600

    WASD movement

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function() {
+
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -14,6 +18,18 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     stdout.write('Exited the game. Good bye now.\n');
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
   }
 };
 
