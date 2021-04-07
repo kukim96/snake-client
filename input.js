fc00bc45af9543d1a00dfa8a9679fd03a486e862
Kiup Kim <kukim96@gmail.com>
2021-04-07 15:13:24 -0600

    created seperate input.js

---
+++ input.js
@@ -0,0 +1,20 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', function (key) {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  const stdout = process.stdout;
+  if (key === '\u0003') {
+    stdout.write('Exited the game. Good bye now.\n');
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
