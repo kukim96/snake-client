860cb62cf702adeba200d7795db5da0424016821
Kiup Kim <kukim96@gmail.com>
2021-04-07 14:14:21 -0600

    received connect function from play.js

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('connect', function () {
+    console.log('You are connected to the game');
+  });
+  conn.on('data', function (data) {
+    console.log(data);
+  });
+
+  return conn;
+}
+
+
