51f006336495595e38ebf6170d11b015299a7e34
Kiup Kim <kukim96@gmail.com>
2021-04-07 15:12:30 -0600

    created seperate input.js

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input')
 console.log('Connecting ...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', function (key) {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  const stdout = process.stdout;
-  if (key === '\u0003') {
-    stdout.write('Exited the game. Good bye now.\n');
-    process.exit();
-  }
-};
-
 setupInput();
-handleUserInput();
+
+ 
